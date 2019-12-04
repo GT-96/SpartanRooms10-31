59306b0e39c57ea487c1cbe1166a06a941553203
ribget <ribget007@gmail.com>
2019-12-03 22:39:03 -0800

    Made changes to the UI. Couldnt use the horizontal scroll bar before. Also working on a seed method

---
+++ SpartanRooms10-31-19/Controllers/SeedController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpartanRoomsData;
+using SpartanRoomsData.Models;
+namespace SpartanRooms10_31_19.Controllers
+{
+    public class SeedController : Controller
+    {
+        private readonly DataContext _context;
+
+        public SeedController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Seed()
+        {
+
+
+
+
+
+
+            return View();
+        }
+    }
+}
